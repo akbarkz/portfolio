7072d5623740f27b1442e3dc0b7f8fde9cb37c36
Akbar Abdrakhmanov <akbar.abdrakhmanov@paxful.com>
2023-08-13 13:51:47 +0600

    update CV link

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ const config = {
     linkedIn: 'https://www.linkedin.com/in/akbarkz/',
     phone: 'https://api.whatsapp.com/send?phone=77077335550',
     email: 'mailto:kz.akbar@gmail.com',
-    cv: 'https://res.cloudinary.com/dlyw63h5s/raw/upload/v1688139391/Akbar_Abdrakhmanov_wnvxmm.docx',
+    cv: 'https://res.cloudinary.com/dlyw63h5s/image/upload/v1691913073/portfolio/Akbar_Abdrakhmanov_hpf55n.pdf',
     logo: 'https://res.cloudinary.com/dlyw63h5s/image/upload/v1691501582/portfolio/logo_pdoaez.png',
   },
   defaultLanguage: Language.EN,
