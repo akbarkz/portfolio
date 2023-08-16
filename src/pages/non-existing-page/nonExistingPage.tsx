682fa852231f74eb51699af17ac692823c969b5d
Akbar Abdrakhmanov <akbar.abdrakhmanov@paxful.com>
2023-08-16 09:22:42 +0600

    refactor 404 page

--- src/pages/non-existing-page/nonExistingPage.tsx
+++ src/pages/non-existing-page/nonExistingPage.tsx
@@ -1,22 +1,16 @@
-import { css } from '@emotion/react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-const wrapperStyles = css`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-`;
+import config from '@config/index';
+
+import { wrapperStyles } from './nonExistingPage.styles';
 
 const NonExistingPage = () => {
   const { t } = useTranslation();
 
   return (
     <div css={wrapperStyles}>
-      <img
-        alt=""
-        src="https://res.cloudinary.com/dlyw63h5s/image/upload/v1690688449/oops_i6auec.png"
-      />
+      <img alt="" className="no-page-image" src={config.urls[404]} />
       <div className="no-page-text">{t('noPage')}</div>
     </div>
   );
