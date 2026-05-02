1eeb7fb7235583f94ec79c317b9317360257b1ed
Akbar Abdrakhmanov <kz.akbar@gmail.com>
2026-05-02 20:32:00 +0500

    fix TS errors and an error in package.json scripts

--- client/src/theme/theme.types.ts
+++ client/src/theme/theme.types.ts
@@ -1,7 +1,7 @@
-import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
+import { ReactElement } from 'react';
 
 export interface IThemeProps {
-  children: ReactJSXElement;
+  children: ReactElement;
 }
 
 interface IBreakpoint {
